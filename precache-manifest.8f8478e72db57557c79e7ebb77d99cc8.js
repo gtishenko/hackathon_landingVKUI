21e90376256558cb68b168aaa75d726eafdeaeea
gtishenko <gregorytyshchenko3011@gmail.com>
2021-04-24 21:17:23 +0300

    Updates

---
+++ precache-manifest.8f8478e72db57557c79e7ebb77d99cc8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c07d4d0b566fb4147eb3",
+    "url": "/hackathon_landingVKUI/static/css/main.7ccc9ca9.chunk.css"
+  },
+  {
+    "revision": "c07d4d0b566fb4147eb3",
+    "url": "/hackathon_landingVKUI/static/js/main.093952aa.chunk.js"
+  },
+  {
+    "revision": "cb3735e92e80dc8281d8",
+    "url": "/hackathon_landingVKUI/static/js/runtime~main.5779193b.js"
+  },
+  {
+    "revision": "68078d9238f392aaf4f1",
+    "url": "/hackathon_landingVKUI/static/css/2.aaac377e.chunk.css"
+  },
+  {
+    "revision": "68078d9238f392aaf4f1",
+    "url": "/hackathon_landingVKUI/static/js/2.3cda732f.chunk.js"
+  },
+  {
+    "revision": "7744ee8f87b9a1d1132439032b86f8f9",
+    "url": "/hackathon_landingVKUI/static/media/vk_logo.7744ee8f.svg"
+  },
+  {
+    "revision": "812615518f097118816b8a66b7af39b4",
+    "url": "/hackathon_landingVKUI/static/media/background.81261551.png"
+  },
+  {
+    "revision": "d1c3b97340082358bff244497bc0d8f8",
+    "url": "/hackathon_landingVKUI/index.html"
+  }
+];
